b292303b7e6e8cc2ea293e9248ea791c18ea44b5
minru <lewminru05@gmail.com>
2025-07-11 22:10:03 +0800

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+//import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
+  plugins: [react(), ghPages()],
+  base: '/lewminru/',
 })
