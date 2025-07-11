3eb48dd9d30faa9c64bcd07ffe7eb98c2af8399a
minru <lewminru05@gmail.com>
2025-07-11 21:20:36 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+/*
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
+*/
+
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link }
+  from 'react-router-dom';
+import './App.css'
+import Home from './Home.jsx';
+//import { Container, AppBar, Toolbar, Typography } from '@mui/material';
+
+/*
+function App() {
+  return (
+    <Router>
+      <header className="app-header">
+        <nav className="navbar">
+          <Link to="/" className="nav-link">About</Link>
+          <Link to="/tutorials" className="nav-link"></Link>
+        </nav>
+      </header>
+
+      <main className="app-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tutorials" element={<Tutorials />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+*/
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/retrofhouse" element={<Tutorials />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function Tutorials() {
+  return <h2>Browse Tutorials</h2>;
+}
+
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link }
+    from 'react-router-dom';
+import './Home.css'
+
+function Home() {
+    return (
+        <div>
+            <header className="app-header">
+                <div className="logo-container">
+                    <a href="#" class="logo">Lew Min Ru</a>
+                </div>
+                <nav className="navbar">
+                    <a href="#home" className="nav-link">About</a>
+                    <a href="#about" className="nav-link">About Me</a>
+                    <a href="#projects" className="nav-link">Projects</a>
+                    <a href="#skills" className="nav-link">Skills</a>
+                    <a href="#contact" className="nav-link">Contact</a>
+                </nav>
+            </header>
+
+            <main>
+                <section id="home" className="intro-header">
+                    <div className="intro-text">
+                        <h2>Hello I'm Min Ru<i className="ph-bold ph-hand-waving"></i></h2>
+                        <h3>I'm a Web Developer</h3>
+                        <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit. Pellentesque id condimentum tortor.</p>
+                        <div className="intro-btns">
+                            <a href="a" className="btn">Contact Info</a>
+                            <a href="a" className="btn">Download Resume</a>
+                        </div>
+                    </div>
+                    <div className="intro-image">
+                        <img src=""></img>
+                    </div>
+                    <h4>About Me</h4>
+                </section>
+
+
+                <section id="about" className="section">
+                    <h2>About</h2>
+                </section>
+
+                <section id="contact" className="section">
+                    <h2>Contact</h2>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default Home;
