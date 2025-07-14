cdbb8ee400e0a31e4edf9df70fb7a405bad07231
minru <lewminru05@gmail.com>
2025-07-15 06:17:35 +0800

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,8 +8,18 @@ import introImage from './images/favicon.png';
 import project1 from './images/retrofhousing_project1.jpg';
 import project2 from './images/pastacreation_project1.jpg';
 import project3 from './images/eKnitwear_project2.jpg';
-import TiltedCard from './reactbits/TiltedCard';
-import html from './icons/html5.png'
+//import TiltedCard from './reactbits/TiltedCard';
+import SpotlightCard from './reactbits/SpotlightCard';
+import htmlIcon from './icons/html5.png';
+import cssIcon from './icons/css.png';
+import jsIcon from './icons/js.png';
+import cSharpIcon from './icons/c-sharp.png';
+import pythonIcon from './icons/python.png';
+import phpIcon from './icons/php.png';
+import reactIcon from './icons/react.png';
+import mysqlIcon from './icons/mysql.png';
+import wpIcon from './icons/wordpress-org.png';
+import xdIcon from './icons/xd.png';
 
 function Home() {
     return (
@@ -41,7 +51,7 @@ function Home() {
                         <p>I'm an Information Technology Student & Aspiring Web Developer with hands-on experience in full stack development, UI/UX design, and digital prototyping. I’m passionate about building clean, user-friendly web solutions that solve real problems and make an impact.</p>
                         <div className="intro-btns">
                             <a href="a" className="btn">Contact Info</a>
-                            <a href="a" className="btn">Download Resume</a>
+                            <a href="/lewminru-resume.pdf" download className="btn">Download Resume</a>
                         </div>
                     </div>
                     <div className="intro-image">
@@ -71,7 +81,77 @@ function Home() {
 
                 <section id="skills" className="skills">
                     <Element name="skills"><h2 className="section-title">🛠️ Skills</h2></Element>
-                   
+                    <div className="skills-section">
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={htmlIcon} alt="htmlIcon" className='skills-icon'></img>
+                                <h5>HTML5</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={cssIcon} alt="cssIcon" className='skills-icon'></img>
+                                <h5>CSS</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={jsIcon} alt="jsicon" className='skills-icon'></img>
+                                <h5>JavaScript</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={cSharpIcon} alt="csharpicon" className='skills-icon'></img>
+                                <h5>C#</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={phpIcon} alt="phpicon" className='skills-icon'></img>
+                                <h5>PHP</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={reactIcon} alt="reacticon" className='skills-icon'></img>
+                                <h5>React</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={pythonIcon} alt="pythonicon" className='skills-icon'></img>
+                                <h5>Python</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={mysqlIcon} alt="pythonicon" className='skills-icon'></img>
+                                <h5>MySql</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={wpIcon} alt="wordpressicon" className='skills-icon'></img>
+                                <h5>WordPress.org</h5>
+                            </div>
+                        </SpotlightCard>
+
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(168, 115, 237, 0.45)">
+                            <div className="skills-item">
+                                <img src={xdIcon} alt="adobexdicon" className='skills-icon'></img>
+                                <h5>AdobeXD</h5>
+                            </div>
+                        </SpotlightCard>
+                    </div>
                 </section>
 
                 <section id="projects" className="projects">
@@ -95,6 +175,7 @@ function Home() {
                                 <h2>Pasta Creation</h2>
                                 <p>
                                     Pasta Recipe app prototype made using Adobe XD
+                                    lorem ipsum lorem ipsum lorem ipsum
                                 </p>
                                 <a href="#">Read More</a>
                             </div>
@@ -107,12 +188,14 @@ function Home() {
                                 <h2>eKnitwear</h2>
                                 <p>
                                     eKnitwear was made with HTML, CSS, and JavaScript / PHP
+                                    lorem ipsum lorem ipsum
                                 </p>
                                 <a href="#">Read More</a>
                             </div>
 
                         </div>
 
+                       
 
 
                     </div>
